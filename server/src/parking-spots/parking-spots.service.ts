ec040c40580466b777bf9a3c47e22cd63627d5cb
Daniil <daniil.shishaev@gmail.com>
2025-04-20 14:36:47 +0300

    date fetching simplification

--- server/src/parking-spots/parking-spots.service.ts
+++ server/src/parking-spots/parking-spots.service.ts
@@ -1,8 +1,8 @@
-import {Injectable, NotFoundException} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Raw, Repository } from 'typeorm';
 import { ParkingSpot } from './entity/parking-spots.entity';
-import { Reservation } from '../reservations/entities/reservations.entity'; 
+import { Reservation } from '../reservations/entities/reservations.entity';
 
 @Injectable()
 export class ParkingSpotsService {
@@ -12,8 +12,8 @@ export class ParkingSpotsService {
         @InjectRepository(Reservation)
         private reservationRepository: Repository<Reservation>,
 
-    ) {}
- 
+    ) { }
+
     async findAll(): Promise<ParkingSpot[]> {
         return this.parkingSpotsRepository.find();
     }
@@ -28,28 +28,28 @@ export class ParkingSpotsService {
     async getAvailableTimes(parkingSpotId: number, date: string) {
         // 1. Get all possible slots (e.g., array of strings)
         const allSlots = [
-          '08:00:00',
-          '09:00:00',
-          '10:00:00',
-          '19:00:00',
+            '08:00:00',
+            '09:00:00',
+            '10:00:00',
+            '19:00:00',
         ];
-      
+
         // 2. Get all reservations for this spot and date
         const reservations = await this.reservationRepository.find({
-            where: { parkingSpot: { id: parkingSpotId }, 
-            // reserved_date: date
-            reserved_date: Raw(alias => `CAST(${alias} AS DATE) = :date`, { date }), 
-            status: 'booked',
-        }, 
+            where: {
+                parkingSpot: { id: parkingSpotId },
+                reserved_date: new Date(date),
+                status: 'booked',
+            },
         });
-      
+
         const bookedSlots = reservations.map(r => r.reserved_time);
-      
+
         // 3. Return array of slots with availability flag
         return allSlots.map(time => ({
-          time,
-          available: !bookedSlots.includes(time),
+            time,
+            available: !bookedSlots.includes(time),
         }));
-      }
-    
+    }
+
 }
